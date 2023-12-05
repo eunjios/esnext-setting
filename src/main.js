31a34d175f28bbbb02857dd79873307d27e6ab8d
Eunji <eeunjiilee@gmail.com>
2023-12-05 11:32:54 +0900

    feat: webpack polyfill

--- src/main.js
+++ src/main.js
@@ -6,3 +6,13 @@ console.log(power(pi, pi));
 const test = new Test();
 console.log(test.foo());
 console.log(test.bar());
+
+console.log(
+  new Promise((resolve, reject) => {
+    setTimeout(() => resolve(1), 1000);
+  })
+);
+
+console.log(Object.assign({}, { x: 1 }, { y: 2 })); // {x: 1, y: 2}
+
+console.log(Array.from([1, 2, 3], (v) => v + v)); // [2, 4, 6]
